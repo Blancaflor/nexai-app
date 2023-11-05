8bc87df41169285b6ead4e9a7d3e43b527d9501a
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-11-05 23:38:56 +0100

    Dev_#10_Commit

---
+++ .history/src/components/header_20231105222102.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import './'
+
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
+    </div>
+  </div>
+)
+
+export default Header
---
+++ .history/src/components/header_20231105222118.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={logo} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
+    </div>
+  </div>
+)
+
+export default Header
