8dd157e82a2661052fd77dbd5c2c354f2d796790
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-11-05 22:15:50 +0100

    Dev_#9_Commit

--- .history/src/components/header_20231105221229.js
+++ .history/src/components/header_20231105221229.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import './Header.css'
 
+
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
